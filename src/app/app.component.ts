f2da5eb5cebba12b6e697bc62bcbdd5de12b5387
Radhouen Assakra <askriradhouen@gmail.com>
2021-05-08 10:56:21 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils/analytics.service';
+import { SeoService } from './@core/utils/seo.service';
+import {NbLayoutDirection, NbLayoutDirectionService} from "@nebular/theme";
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
+
+  constructor(private analytics: AnalyticsService, private  directionService: NbLayoutDirectionService, private seoService: SeoService) {
+  }
+
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
+    this.seoService.trackCanonicalChanges();
+    this.directionService.setDirection(NbLayoutDirection.RTL);
+  }
+}
